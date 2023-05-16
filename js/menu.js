13f8429aa88d1ec6ddea643ac070b3c4e2405df6
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-05-16 14:32:45 +0300

    Fix the menu icon and hr positioning and fix eslint errors

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,16 @@
-function mobile_menu() {
-  var x = document.getElementById("top_navigation");
-  var y = document.querySelector('#ham_button');
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    y.src = "./icons/Union.png";
+function mobileMenu() {
+  const x = document.getElementById('top_navigation');
+  const y = document.querySelector('#ham_button');
+  const z = document.querySelector('.menu_icon h2');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    z.style.visibility = 'visible';
+    y.src = './icons/Union.png';
   } else {
-    x.style.display = "block";
-    y.src = "./icons/close.svg";
+    x.style.display = 'block';
+    z.style.visibility = 'hidden';
+    y.src = './icons/close.svg';
   }
 }
+
+mobileMenu();
