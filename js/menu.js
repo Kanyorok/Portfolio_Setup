96968e99959744c79fd4a29fe11ef27eff396cb1
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-05-17 11:16:46 +0300

    Add Javascript object for popup window

--- js/menu.js
+++ js/menu.js
@@ -13,4 +13,48 @@ function mobileMenu() {
   }
 }
 
-mobileMenu();
+const projects = {
+  projectOne: {
+    projectTitle: "HTML and JavaScript",
+    projectDescription: "The project is about the coding of javascript and HTML",
+  },
+  projectTwo: {
+    projectTitle: "Ruby on Rails",
+    projectDescription: "It is an illustration of the projects done in relation to Ruby"
+  },
+  projectThree: {
+    projectTitle: "Reactjs",
+    projectDescription: "The project is about the application of Reactjs"
+  },
+  projectFour: {
+    projectTitle: "CSS Frameworks",
+    projectDescription: "The project is about the use of CSS Frameworks"
+  },
+  projectFive: {
+    projectTitle: "Mongodb",
+    projectDescription: "The project is about the use of databases in websites"
+  },
+  projectSix: {
+    projectTitle: "Nextjs",
+    projectDescription: "The project is about the coding website using Nextjs"
+  },
+  projectSeven: {
+    projectTitle: "Nodejs",
+    projectDescription: "The project is about the coding website using Nodejs"
+  },
+}
+
+function clickedButton(the_id) {
+  let savedInfo = the_id;
+  let titleSelect = document.getElementById('headingSection');
+  let paragraphSelect = document.getElementById('projectDesc');
+
+
+  for (let project in projects ){
+    if(project === savedInfo){
+      titleSelect.innerHTML = projects[project].projectTitle;
+      paragraphSelect.innerHTML = projects[project].projectDescription;
+    }
+  }
+}
+
