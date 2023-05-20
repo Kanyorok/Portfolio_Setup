89359be2778f3e733856044209ce2b50cb770303
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-05-20 08:49:13 +0300

    Add email validation to form

--- js/menu.js
+++ js/menu.js
@@ -1,107 +1,109 @@
 function mobileMenu() {
-  const x = document.getElementById("top_navigation");
-  const y = document.querySelector("#ham_button");
-  const z = document.querySelector(".menu_icon h2");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    z.style.visibility = "visible";
-    y.src = "./icons/Union.png";
+  const x = document.getElementById('top_navigation');
+  const y = document.querySelector('#ham_button');
+  const z = document.querySelector('.menu_icon h2');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+    z.style.visibility = 'visible';
+    y.src = './icons/Union.png';
   } else {
-    x.style.display = "block";
-    z.style.visibility = "hidden";
-    y.src = "./icons/close.svg";
+    x.style.display = 'block';
+    z.style.visibility = 'hidden';
+    y.src = './icons/close.svg';
   }
 }
 
-const bannerProjects = {
-  projectOne: {
-    projectTitle: "Multi-Post Stories",
+const bannerProjects = [
+  {
+    Name: 'projectOne',
+    projectTitle: 'Multi-Post Stories',
     projectDescription:
       `A daily selection of privately personalized reads; no accounts or
       sign-ups required. has been the industry's standard dummy text
       ever since the 1500s, when an unknown printer took a standard
       dummy text.`,
-    tech: ["CSS","HTML", "Bootstrap", "Ruby"],
-    img: "icons/Works_cont.svg",
-  }
-};
+    tech: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/Works_cont.svg',
+  },
+];
 
 const projectInfo = [
   {
     id: 1,
-    projectName: "Data Dashboard Healthcare",
+    projectName: 'Data Dashboard Healthcare',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/pro_sec.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/pro_sec.svg',
   },
   {
     id: 2,
-    projectName: "Data Dashboard Healthcare",
+    projectName: 'Data Dashboard Healthcare',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/Data_content_image.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/Data_content_image.svg',
   },
   {
     id: 3,
-    projectName: "Website Portfolio",
+    projectName: 'Website Portfolio',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/website.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/website.svg',
   },
   {
     id: 4,
-    projectName: "Profesional Art Printing Data",
+    projectName: 'Profesional Art Printing Data',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/professional.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/professional.svg',
   },
   {
     id: 5,
-    projectName: "Data Dashboard Healthcare",
+    projectName: 'Data Dashboard Healthcare',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/Data_content_image.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/Data_content_image.svg',
   },
   {
     id: 6,
-    projectName: "Website Portfolio",
+    projectName: 'Website Portfolio',
     projectInfo:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    tech: ["HTML", "Bootstrap", "Ruby"],
-    img: "icons/website.svg",
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    img: 'icons/website.svg',
   },
 ];
 
-function mainProject(){
+function mainProject() {
+  const presentProject = bannerProjects[0];
   const headlineProject = `<section class="works_content">
-  <img src=${bannerProjects.projectOne.img} class="image_placement" alt="The Banner" />
+  <img src=${presentProject.img} class="image_placement" alt="The Banner" />
   <div class="text_placement">
-    <h2>${bannerProjects.projectOne.projectTitle}</h2>
-    <p>${bannerProjects.projectOne.projectDescription}</p>
+    <h2>${presentProject.projectTitle}</h2>
+    <p>${presentProject.projectDescription}</p>
     <ul>
       <li>
-        <a href="#">${bannerProjects.projectOne.tech[0]}</a>
+        <a href="#">${presentProject.tech[0]}</a>
       </li>
       <li>
-        <a href="#">${bannerProjects.projectOne.tech[1]}</a>
+        <a href="#">${presentProject.tech[1]}</a>
       </li>
       <li>
-        <a href="#">${bannerProjects.projectOne.tech[2]}</a>
+        <a href="#">${presentProject.tech[2]}</a>
       </li>
       <li>
-        <a href="#">${bannerProjects.projectOne.tech[3]}</a>
+        <a href="#">${presentProject.tech[3]}</a>
       </li>
     </ul>
-    <button id="projectOne" onclick="clickedButton(this.id)" type="button">See project</button>
+    <button id=${presentProject.Name} onclick="clickedButton(this.id)" type="button">See project</button>
   </div>
-</section>`
+</section>`;
 
-return headlineProject;
+  return headlineProject;
 }
 
 const showSections = () => {
@@ -130,8 +132,8 @@ const showSections = () => {
       </div>
     </section>
   </section>`;
-    } else {
-      return `<section class="center_content">
+    }
+    return `<section class="center_content">
   <section class="content_desc">
     <img src=${item.img} alt="" />
     <div class="top_left">
@@ -154,14 +156,13 @@ const showSections = () => {
     </div>
   </section>
 </section>`;
-    }
   });
 
   return divCard;
 };
 
 // The popup window
-function showPopup(){
+function showPopup() {
   const pop = `<div class="popup">
   <div class="popup_content">
     <div class="titleContent">
@@ -186,106 +187,80 @@ function showPopup(){
     </ul>
     <div class="desktopView">
       <div id="popup_info">
-        <img src="icons/Snapshoot Portfolio.jpg" alt="Portfolio">
+        <img id="popup-img" src="icons/Snapshoot Portfolio.jpg" alt="Portfolio">
       </div>
       <div class ="all_info">
         <p id="projectDesc"></p>
         <div class="bottom_section">
-          <button id="projectSeven" type="button">See project <img src="icons/live_icon.svg" alt=""></button>
-          <button id="projectSeven" type="button">See project <img src="icons/github_2.svg"> </button>
+          <button id="projectSeven" type="button">See project <img src="icons/live_icon.svg" alt="" class="btnIcon"></button>
+          <button id="projectSeven" type="button">See project <img src="icons/github_2.svg" class="btnIcon"> </button>
         </div>
       </div>
     </div>
   </div>
-</div>`
+</div>`;
 
-return pop;
-
+  return pop;
 }
 
+function clickedButton(theId) {
+  const savedInfo = theId;
+  const titleSelect = document.getElementById('headingSection');
+  const paragraphSelect = document.getElementById('projectDesc');
+  const popWin = document.querySelector('.popup');
 
-function clickedButton(the_id) {
-  let savedInfo = the_id;
-  let titleSelect = document.getElementById("headingSection");
-  let paragraphSelect = document.getElementById("projectDesc");
-  const images = document.querySelector(".popup_info ");
-  const popWin = document.querySelector(".popup");
-
-  for (let project = 0; project < projectInfo.length; project++) {
+  projectInfo.forEach((e, i) => {
+    const project = i;
     if (projectInfo[project].id === Number(savedInfo)) {
       titleSelect.innerHTML = projectInfo[project].projectName;
       paragraphSelect.innerHTML = projectInfo[project].projectInfo;
-      popWin.style.visibility = "visible";
-    } 
-  }
-
-  for (let bannerPrj in bannerProjects ){
-    if(bannerPrj === savedInfo){
-      titleSelect.innerHTML = bannerProjects[bannerPrj].projectTitle;
-      paragraphSelect.innerHTML = bannerProjects[bannerPrj].projectDescription;
-      images.src= bannerProjects[bannerPrj].
-      popWin.style.visibility = "visible";
+      popWin.style.visibility = 'visible';
     }
-  }
+  });
 
+  bannerProjects.forEach((bannerPrj) => {
+    if (bannerPrj.Name === savedInfo) {
+      titleSelect.innerHTML = bannerPrj.projectTitle;
+      paragraphSelect.innerHTML = bannerPrj.projectDescription;
+      popWin.style.visibility = 'visible';
+    }
+  });
 }
 
-//close popup window
+clickedButton();
+
+// close popup window
 function closed() {
-  const popWin = document.querySelector(".popup");
-  popWin.style.visibility = "hidden";
+  const popWin = document.querySelector('.popup');
+  popWin.style.visibility = 'hidden';
 }
 
 function validateEmail(event) {
   event.preventDefault();
-  let email = document.forms[0].elements['Email'].value
+  const email = document.forms[0].elements.Email.value;
   // check email
   const res = email.match(/[A-Z]/g);
-  const btn = document.querySelector('form #form_btn')
-  const container = document.querySelector("form #form-btn-container");
-  const child = document.createElement('small')
+  const btn = document.querySelector('form #form_btn');
+  const container = document.querySelector('form #form-btn-container');
+  const child = document.createElement('small');
   let msg = '';
-  
-  alert('OK');
-  if (res) msg = "email should be lowercase only"
-  else return null
+
+  if (res) msg = 'Error:Form was not Sent! The email should be lowercase only';
+  else return null;
   child.innerText = msg;
-  child.style.color = 'red'; 
+  child.style.color = 'red';
   container.insertBefore(child, btn);
+  return email;
 }
 
 window.onload = () => {
-  document.querySelector('#form').addEventListener('submit', (event) => validateEmail(event));  
-}
+  document.querySelector('#form').addEventListener('submit', (event) => validateEmail(event));
+};
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   document.getElementById('multiStories').innerHTML = mainProject();
   document.getElementById('popWindow').innerHTML = showPopup();
-  document.getElementById("grid_contentdesc").innerHTML =
-    showSections().join("");
-  
+  document.getElementById('grid_contentdesc').innerHTML = showSections().join('');
 });
-
-function validateEmail(event) {
-  event.preventDefault();
-  let email = document.forms[0].elements['Email'].value
-  // check email
-  const res = email.match(/[A-Z]/g);
-  const btn = document.querySelector('form #form_btn')
-  const container = document.querySelector("form #form-btn-container");
-  const child = document.createElement('small')
-  let msg = '';
-  
-  if (res) msg = "Error:Form was not Sent! The email should be lowercase only"
-  else return null
-  child.innerText = msg;
-  child.style.color = 'red'; 
-  container.insertBefore(child, btn);
-}
-
-window.onload = () => {
-  document.querySelector('#form').addEventListener('submit', (event) => validateEmail(event));  
-}
-
 mobileMenu();
 closed();
