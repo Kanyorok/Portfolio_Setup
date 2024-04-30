c95de42e84db2991971221008688cb2f960170ab
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-04-30 10:02:00 +0300

    Reduce text description of projects

--- js/menu.js
+++ js/menu.js
@@ -34,7 +34,7 @@ const projectInfo = [
     id: 1,
     projectName: 'Budget Application',
     projectInfo:
-      "RubyBudget is a cutting-edge personal budget app meticulously crafted using the power and elegance of Ruby on Rails. With its sleek design and intuitive interface, RubyBudget empowers users to take control of their finances effortlessly. Track your income and expenses, set budget goals, and receive insightful financial reports.",
+      "With its sleek design and intuitive interface, RubyBudget empowers users to track income and expenses, set budget goals, and receive insightful financial reports.",
     tech: ['HTML', 'RoR', 'Postgres', 'CSS'],
     img: 'icons/budget_app.png',
   },
